bb70533bb895639ea0b5053be74f97eb23dd0e66
rizkhal <lamaaurizkhal@gmail.com>
2021-10-23 23:47:32 +0800

    works but logical falacy

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,199 @@
-/*
-  Usage:
-  1. npm install //To install all dev dependencies of package
-  2. npm run dev //To start development and server for live preview
-  3. npm run prod //To generate minifed files for live server
-*/
-
-const { src, dest, task, watch, series, parallel } = require("gulp");
-const del = require("del"); //For Cleaning build/dist for fresh export
-const options = require("./config"); //paths and other options from config.js
+/**
+ * Imports
+ */
+const { src, dest, watch, parallel } = require("gulp");
 const browserSync = require("browser-sync").create();
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const imagemin = require("gulp-imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const nunjucks = require("gulp-nunjucks");
+const color = require("gulp-color");
+const nodePath = require("path");
 
-const postcss = require("gulp-postcss"); //For Compiling tailwind utilities with tailwind config
-const concat = require("gulp-concat"); //For Concatinating js,css files
-const uglify = require("gulp-terser"); //To Minify JS files
-const imagemin = require("gulp-imagemin"); //To Optimize Images
-const cleanCSS = require("gulp-clean-css"); //To Minify CSS files
-const purgecss = require("gulp-purgecss"); // Remove Unused CSS from Styles
+/**
+ * Configuration
+ * @type {String}
+ */
+var cssDir = "./src/css",
+  jsDir = "./src/js",
+  htmlDir = "./src/views",
+  imgDir = "./src/img";
 
-//Note : Webp still not supported in major browsers including firefox
-//const webp = require('gulp-webp'); //For converting images to WebP format
-//const replace = require('gulp-replace'); //For Replacing img formats to webp in html
-const logSymbols = require("log-symbols"); //For Symbolic Console logs :) :P
+/**
+ * Helpers
+ */
 
-//Load Previews on Browser on dev
-function livePreview(done) {
+function _compile_html(path, onEnd, log = true, ret = false) {
+  if (log) _log("[HTML] Compiling: " + path, "GREEN");
+
+  let compile_html = src(path, { base: htmlDir })
+    .pipe(plumber())
+    .pipe(
+      nunjucks.compile(
+        {
+          version: "1.0",
+          site_name: "Ayuwind",
+        },
+        /**
+         * Nunjucks options
+         */
+        {
+          trimBlocks: true,
+          lstripBlocks: true,
+          /**
+           * Nunjucks filters
+           * @type {Object}
+           */
+          filters: {
+            is_active: (str, reg, page) => {
+              reg = new RegExp(reg, "gm");
+              reg = reg.exec(page);
+              if (reg != null) {
+                return str;
+              }
+            },
+          },
+        }
+      )
+    )
+    .pipe(
+      rename({
+        dirname: "",
+        extname: ".html",
+      })
+    )
+    .on("error", console.error.bind(console))
+    .on("end", () => {
+      if (onEnd) onEnd.call(this);
+
+      if (log) _log("[HTML] Finished", "GREEN");
+    })
+    .pipe(dest("pages"))
+    .pipe(plumber.stop());
+
+  if (ret) return compile_html;
+}
+
+function _compile_css(path, onEnd, log = true, ret = false) {
+  if (log) _log("[SCSS] Compiling:" + path, "GREEN");
+
+  let compile_css = src(path)
+    .pipe(plumber())
+    .on("error", console.error.bind(console))
+    .on("end", () => {
+      if (onEnd) onEnd.call(this);
+
+      if (log) _log("[SCSS] Finished", "GREEN");
+    })
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest(cssDir))
+    .pipe(plumber.stop());
+
+  if (ret) return compile_css;
+}
+
+function _log(str, clr) {
+  if (!clr) clr = "WHITE";
+  console.log(color(str, clr));
+}
+
+/**
+ * End of helper
+ */
+
+/**
+ * Execution
+ */
+
+function folder() {
+  return src("*.*", { read: false })
+    .pipe(dest("./assets/css"))
+    .pipe(dest("./assets/js"))
+    .pipe(dest("./assets/img"));
+}
+
+function image() {
+  return src(imgDir + "/**/*.*")
+    .pipe(plumber())
+    .pipe(imagemin([imageminMozjpeg({ quality: 80 })]))
+    .pipe(dest(imgDir))
+    .pipe(plumber.stop());
+}
+
+function compile_css() {
+  return _compile_css(cssDir + "/**/*.css", null, false, true);
+}
+
+function compile_html() {
+  return _compile_html(htmlDir + "/**/*.njk", null, false, true);
+}
+
+function watching() {
+  compile_css();
+  compile_html();
+
+  /**
+   * BrowserSync initialization
+   * @type {Object}
+   */
   browserSync.init({
     server: {
-      baseDir: options.paths.dist.base,
+      baseDir: "./",
     },
-    port: options.config.port || 5000,
+    startPath: "pages/index.html",
+    port: 8080,
   });
-  done();
+
+  /**
+   * Watch ${htmlDir}
+   */
+  watch([
+    htmlDir + "/**/*.njk",
+    cssDir + "/**/*.css",
+    jsDir + "/**/*.js",
+    imgDir + "/**/*.*",
+  ]).on("change", (file) => {
+    file = file.replace(/\\/g, nodePath.sep);
+
+    if (file.indexOf(".css") > -1) {
+      _compile_css(cssDir + "/**/*.css", () => {
+        return browserSync.reload();
+      });
+    }
+
+    if (file.indexOf("layouts") > -1 && file.indexOf(".njk") > -1) {
+      _compile_html(htmlDir + "/*.njk", () => {
+        return browserSync.reload();
+      });
+    } else if (file.indexOf(".njk") > -1) {
+      _compile_html(file, () => {
+        return browserSync.reload();
+      });
+    }
+
+    if (file.indexOf(jsDir) > -1 || file.indexOf(imgDir) > -1) {
+      return browserSync.reload();
+    }
+  });
 }
 
-// Triggers Browser reload
-function previewReload(done) {
-  console.log("\n\t" + logSymbols.info, "Reloading Browser Preview.\n");
-  browserSync.reload();
-  done();
-}
+// Create folder first
+exports.folder = folder;
 
-//Development Tasks
-function devHTML() {
-  return src(`./src/views/**/*.html`).pipe(dest(options.paths.dist.base));
-}
+// Minify images
+exports.image = image;
 
-function devStyles() {
-  const tailwindcss = require("tailwindcss");
-  return src(`./src/css/*.css`)
-    .pipe(dest(options.paths.src.css))
-    .pipe(
-      postcss([tailwindcss(options.config.tailwindjs), require("autoprefixer")])
-    )
-    .pipe(concat({ path: "styles.css" }))
-    .pipe(dest(options.paths.dist.css));
-}
+// Compile SCSS
+exports.css = compile_css;
 
-function devScripts() {
-  return src([`./src/js/*.js`])
-    .pipe(concat({ path: "scripts.js" }))
-    .pipe(dest(options.paths.dist.js));
-}
+// Compile HTML
+exports.html = compile_html;
 
-function devImages() {
-  return src(`./src/img/*`).pipe(dest(options.paths.dist.img));
-}
+// Dist
+exports.dist = parallel(folder, compile_css, compile_html);
 
-function watchFiles() {
-  watch(`./src/views/**/*.html`, series(devHTML, devStyles, previewReload));
-  watch(
-    [options.config.tailwindjs, `./src/css/*.css`],
-    series(devStyles, previewReload)
-  );
-  watch(`${options.paths.src.js}/**/*.js`, series(devScripts, previewReload));
-  watch(`${options.paths.src.img}/**/*`, series(devImages, previewReload));
-  console.log("\n\t" + logSymbols.info, "Watching for Changes..\n");
-}
-
-function devClean() {
-  console.log(
-    "\n\t" + logSymbols.info,
-    "Cleaning dist folder for fresh start.\n"
-  );
-  return del([options.paths.dist.base]);
-}
-
-exports.default = series(
-  devClean, // Clean Dist Folder
-  parallel(devStyles, devScripts, devImages, devHTML), //Run All tasks in parallel
-  livePreview, // Live Preview Build
-  watchFiles // Watch for Live Changes
-);
+// Run this command for dev.
+exports.default = watching;
